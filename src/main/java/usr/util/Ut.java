49821926a151aaef09ee2393a04be8e64189fafc
kimdonghyeon <sh2115521@naver.com>
2022-07-28 14:19:08 +0900

    init

--- src/main/java/usr/util/Ut.java
+++ src/main/java/usr/util/Ut.java
@@ -1,15 +1,39 @@
 package usr.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+import java.util.Map;
 
 public class Ut {
     public static class json {
-        public static String toJsonStr(Object obj, String defaultValue) {
-            ObjectMapper om = new ObjectMapper();
+        private static final ObjectMapper om;
 
+        static {
+            om = new ObjectMapper();
+        }
+
+        public static String toStr(Object obj, String defaultValue) {
             try {
                 return om.writeValueAsString(obj);
+            } catch (JsonProcessingException e) {
+                return defaultValue;
+            }
+        }
+
+        public static <T> T toObj(String jsonStr, Class<T> cls, T defaultValue) {
+            try {
+                return om.readValue(jsonStr, cls);
+            } catch (JsonProcessingException e) {
+                return defaultValue;
+            }
+        }
+
+        public static <T> T toObj(String jsonStr, TypeReference<T> typeReference, T defaultValue) {
+            try {
+                return om.readValue(jsonStr, typeReference);
             } catch (JsonProcessingException e) {
                 return defaultValue;
             }
